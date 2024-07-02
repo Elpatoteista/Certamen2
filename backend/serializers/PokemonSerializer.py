152231a55563a6ee05dbb6b43f4a8eb797f96f9e
Elpatoteista <patricioaylwinretamal@gmail.com>
2024-07-01 21:16:50 -0400

    cert2

---
+++ backend/serializers/PokemonSerializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from backend.models import Pokemon
+
+class PokemonSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=20)
+    pokedex_number = serializers.IntegerField(required=False)
+    primary_type = serializers.CharField(max_length=100, required=False)
+    secondary_type = serializers.CharField(max_length=100, required=False)
+    Imagen_pokemon = serializers.ImageField(required=True)
+
+    class Meta:
+        model = Pokemon
+        fields = ['name', 'pokedex_number', 'primary_type', 'secondary_type','Imagen_pokemon']
